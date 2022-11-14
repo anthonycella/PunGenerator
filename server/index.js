1c127a40a971ba2175131bbc8fdb13db2b3b75fa
Anthony Cella <anthony.cella242@gmail.com>
2022-11-14 10:47:46 -0500

    Boilerplate activated

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+const PORT = 3000;
+
+app.listen(PORT);
+console.log(`Server listening at http://localhost:${PORT}`);
