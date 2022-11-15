c16e46ceb739e28d3474c4cd724f66792a1b0d9f
Anthony Cella <anthony.cella242@gmail.com>
2022-11-15 10:36:46 -0500

    Got more work done on the back end

--- server/thesaurus.js
+++ server/thesaurus.js
@@ -2,18 +2,46 @@ const axios = require('axios');
 
 
 function getPunnableWords(word) {
-  axios(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${process.env.THESAURUS_API_KEY}`)
-    .then((response) => {
+  getPartsOfSpeech(word)
+    .then((partsOfSpeech) => {
       const punnableWords = {};
 
+      for (const partOfSpeech of partsOfSpeech) {
+        punnableWords[partOfSpeech] = [];
+      }
+
+      axios(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${process.env.THESAURUS_API_KEY}`)
+        .then((response) => {
+
+          for (const result of response) {
+            const punnableWordResponses = [];
+            const information = result['meta'];
+            const word = trimWordIfNeeded(information['id']);
+            const partOfSpeech = information['fl'];
+
+            for (const part of partsOfSpeech) {
+              if (part !== partOfSpeech) {
+                punnableWords[part].push(word);
+              }
+            }
+          }
+        });
+    })
+}
+
+function getPartsOfSpeech(word) {
+  const partsOfSpeech = [];
+  axios(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.DICTIONARY_API_KEY}`)
+    .then((response) => {
       for (const result of response) {
-        const punnableWordResponses = [];
         const information = result['meta'];
         const partOfSpeech = information['fl'];
 
-
+        partsOfSpeech.push(partOfSpeech);
       }
-    })
+
+      return partsOfSpeech;
+    });
 }
 
 function isPunnableWord(word, partOfSpeechOfOriginal) {
@@ -30,6 +58,16 @@ function isPunnableWord(word, partOfSpeechOfOriginal) {
 
       return false;
     });
+}
+
+function trimWordIfNeeded(word) {
+  for (let currentIndex = 0; currentIndex < word.length; currentIndex++) {
+    if (word[currentIndex] === ':') {
+      return word.slice(0, currentIndex);
+    }
+  }
+
+  return word;
 }
 
 
