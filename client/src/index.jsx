e453330c659644903551cbbe7a83e65444d20c33
NaNdyKing <andyking.lrn@gmail.com>
2022-11-16 11:16:09 -0500

    front end structure;

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div>Pun Generator</div>,
+root.render (
+  <>
+    <h1>Pun Generator</h1>
+    <App />
+  </>
 );
